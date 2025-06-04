c490071639199116ed6a072247e6cfbab08fe8c6
AngelCastillaSandoval <angel.castilla@vallegrande.edu.pe>
2025-06-04 13:04:47 -0500

    frontend

---
+++ src/environments/environments.ts
@@ -0,0 +1,40 @@
+export const environment = {
+    production: false,
+    //Team_01
+    ms_family: 'https://vg-ms-family-b4go.onrender.com',
+    ms_person: 'https://vg-ms-persons.onrender.com',
+
+    //Team_02
+    ms_beneficiario: 'https://ms-beneficiary.onrender.com',
+    ms_beneficiario_education: 'https://ms-education.onrender.com',
+    ms_beneficiario_health: 'https://ms-health.onrender.com',
+
+    //Team_03
+    ms_issue: 'https://ms-issue.onrender.com',
+    ms_workshop: 'https://workshops-7wkh.onrender.com',
+    ms_attendance: 'https://asistenciav1.onrender.com',
+
+    //ms_attendance: 'https://ms-attendance.onrender.com',
+    //ms_issue: 'https://issue-mkcl.onrender.com',
+    //ms_workshop: 'https://workshop-8fvg.onrender.com',
+
+    //Team_04
+    ms_goal: 'https://vg-ms-transformation-plan.onrender.com',
+    ms_assist: 'https://vg-ms-transformation-plan.onrender.com',
+    ms_tranformation: 'https://vg-ms-transformation-plan.onrender.com',
+
+    //Team_05
+    ms_user: 'https://ms-render-users.onrender.com',
+    ms_report: 'https://ms-render-report.onrender.com',
+    ms_report_workshop: 'https://ms-render-report-workshop.onrender.com',
+    ms_activity: 'https://ms-render-actividad.onrender.com',
+
+    firebaseConfig: {
+        apiKey: "AIzaSyAD3fqr_JmccCJUnHbTWt5iu_C3o_84KOc",
+        authDomain: "security-prs1.firebaseapp.com",
+        projectId: "security-prs1",
+        storageBucket: "security-prs1.firebasestorage.app",
+        messagingSenderId: "437924195694",
+        appId: "1:437924195694:web:2dda06786d4c317d97257f"
+    }
+};
